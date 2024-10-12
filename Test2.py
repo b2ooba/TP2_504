9c8113d3550cc54a4d41d57a70cc309af4f0a9a3
b2ooba <dialloguelin23@gmail.com>
2024-10-12 14:48:14 +0200

    Test qui prend n1 et n2 en paramètre

--- Test2.py
+++ Test2.py
@@ -19,4 +19,24 @@ class FizzBuzz:
         if n == 0 or n == 1:
             return 1
         else:
+            return n * self.factorielle(n - 1) 
+    print("\n")
+    def affiche_n(n1, n2):
+        result = ""
+        for i in range(n1, n2 + 1):
+            if i % 15 == 0:
+                result += "FrisBee"
+            elif i % 3 == 0:
+                result += "Fizz"
+            elif i % 5 == 0:
+                result += "Buzz"
+            else:
+                result += str(i)
+        return result 
+    print("La reponse pour n1 et n2 est :") 
+    print(affiche_n(5, 10))
+    def factorielle(self, n):
+        if n == 0 or n == 1:
+            return 1
+        else:
             return n * self.factorielle(n - 1)

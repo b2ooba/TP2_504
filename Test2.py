00146a10bbc0a1aaf8c3155bd92ed1e46ef2342b
b2ooba <dialloguelin23@gmail.com>
2024-10-12 14:58:37 +0200

    Fichier finale contenant toute la question 1

--- Test2.py
+++ Test2.py
@@ -1,7 +1,6 @@
 class FizzBuzz:
     def __init__(self, name):
         self.name = name
-
     def affiche():
         result = ""
         for i in range(1, 101):
@@ -19,9 +18,28 @@ class FizzBuzz:
         if n == 0 or n == 1:
             return 1
         else:
-            return n * self.factorielle(n - 1) 
+            return n * self.factorielle(n - 1)
     print("\n")
-    def affiche_n(n1, n2):
+    def affiche_n(self):
+        result = ""
+        for i in range(1, 101):
+            if i % 15 == 0:
+                result += "FrisBee"  
+            elif i % 3 == 0:
+                result += "Fizz"
+            elif i % 5 == 0:
+                result += "Buzz"
+            else:
+                result += str(i) + ""
+        print(result)
+    affiche_n(15)
+    def factorielle(self, n):
+        if n == 0 or n == 1:
+            return 1
+        else:
+            return n * self.factorielle(n - 1)
+    print("\n")
+    def affiche_nn(n1, n2):
         result = ""
         for i in range(n1, n2 + 1):
             if i % 15 == 0:
@@ -34,7 +52,7 @@ class FizzBuzz:
                 result += str(i)
         return result 
     print("La reponse pour n1 et n2 est :") 
-    print(affiche_n(5, 10))
+    print(affiche_nn(5, 10))
     def factorielle(self, n):
         if n == 0 or n == 1:
             return 1
